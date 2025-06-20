c114a9695a15522672363579a7aa743e9480b817
Thiwanka Somachandra <thivankas@outlook.com>
2025-06-20 05:41:14 +0300

    solution for cannon ball projectile

---
+++ mathematics/projectile.h
@@ -0,0 +1,18 @@
+# ifndef FT_PROJECTILE_H
+# define FT_PROJECTILE_H
+
+# include "tuple.h"
+
+typedef struct s_projectile
+{
+	t_tuple *point;
+	t_tuple *velocity;
+} t_projectile;
+
+typedef struct s_environment
+{
+	t_tuple *gravity;
+	t_tuple *wind;
+} t_environment;
+
+# endif
