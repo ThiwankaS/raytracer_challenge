0d911a02a3f40a98b0cac4f2284168af1d259f82
Thiwanka Somachandra <thivankas@outlook.com>
2025-06-23 05:05:32 +0300

    completion of drawing the clock face on the canvas

---
+++ transformation/canvas.h
@@ -0,0 +1,21 @@
+# ifndef FT_CANVAS_H
+# define FT_CANVAS_H
+
+# include <string.h>
+# include "tuple.h"
+# include "./ft_libft/libft.h"
+# include <fcntl.h>
+
+typedef struct s_canvas
+{
+	int	width;
+	int height;
+	t_tuple **pixels;
+} t_canvas;
+
+void write_pixel(t_canvas *canvas, int x, int y, t_tuple *color);
+t_canvas *init_canvas(int width, int height);
+
+void canvs_to_ppm(t_canvas *canvas, int fd);
+
+# endif
