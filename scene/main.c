2431ae178cdde34238ee915d8437beabc84aad86
Thiwanka Somachandra <thivankas@outlook.com>
2025-07-03 09:01:00 +0300

    correct up data yep in the main

--- scene/main.c
+++ scene/main.c
@@ -4,19 +4,20 @@ int main(void)
 {
 	t_world *world = world_init();
 	t_camera *camera = camera_init(11, 11, PI/2);
-	t_tuple *from = point(0,0,-5);
-	t_tuple *to   = point(0,0,1);
-	t_tuple *up   = point(0,1,0);
 
-	printTuple(from, "from");
-	printTuple(to, "to");
-	printTuple(up, "up");
+	t_tuple *from = point(0,0,-5);
+	t_tuple *to   = point(0,0,0);
+	t_tuple *up   = vector(0,1,0);
 	t_matrix *m = view_transformation(from, to, up);
+	matrix_free(camera->transform);
+	camera->transform = NULL;
+	camera->transform = m;
 
 	t_canvas *image = render(camera, world);
-	t_tuple *pixel = image->pixels[5];
-	printTuple(pixel, "pixel");
-	matrix_print(m);
+
+	// t_tuple *pixel = image->pixels[5];
+	// printTuple(pixel, "pixel");
+
 	matrix_free(camera->transform);
 	free(camera);
 	free(world->light->color);
